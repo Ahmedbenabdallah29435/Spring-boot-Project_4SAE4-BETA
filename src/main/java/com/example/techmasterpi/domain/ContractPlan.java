b08fe37b81b820b21882f1db2b9b8336fea51f0a
dhaouadimedhabib <medhabib.dhaouadi@esprit.tn>
2023-02-20 01:01:11 +0100

    planmangement
    planmangement

---
+++ src/main/java/com/example/techmasterpi/domain/ContractPlan.java
@@ -0,0 +1,41 @@
+package com.example.techmasterpi.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class ContractPlan {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer contractId;
+
+    @Column
+
+    private LocalDate contractDate;
+
+    @Column
+    private Integer price;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plan_user_id")
+    private User planUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plan_contract_plan_id")
+    private Plan planContractPlan;
+
+}
---
+++ src/main/java/com/example/techmasterpi/domain/SellerOffer.java
@@ -0,0 +1,57 @@
+package com.example.techmasterpi.domain;
+
+
+import com.example.techmasterpi.model.TypeOffer;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.Set;
+
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class SellerOffer {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer sellid;
+
+    @Column
+    private String descrption;
+
+    @Column
+    private String title;
+
+    @Column
+    private Double price;
+
+    @Column
+    private String adress;
+
+    @Column
+    private LocalDate datesell;
+
+    @Column
+    private String picture;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private TypeOffer typeoffer;
+
+
+    @OneToMany(mappedBy = "contractSell")
+    private Set<SellContract> contractSellSellContracts;
+
+
+
+}
